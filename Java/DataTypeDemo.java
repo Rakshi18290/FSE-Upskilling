21110368a695cc5b2ba3cac86194777a7a6a09ca
Rakshi18290 <vsrakshithablr@gmail.com>
2025-05-28 09:05:15 +0530

    Create DataTypeDemo.java

---
+++ Java/DataTypeDemo.java
@@ -0,0 +1,15 @@
+public class DataTypeDemo {
+    public static void main(String[] args) {
+        int intVal = 10;
+        float floatVal = 3.14f;
+        double doubleVal = 5.6789;
+        char charVal = 'A';
+        boolean boolVal = true;
+
+        System.out.println("int: " + intVal);
+        System.out.println("float: " + floatVal);
+        System.out.println("double: " + doubleVal);
+        System.out.println("char: " + charVal);
+        System.out.println("boolean: " + boolVal);
+    }
+}
