3142198b611b5011362d84268785e0bd1cac39c1
Rakshi18290 <vsrakshithablr@gmail.com>
2025-05-28 09:16:52 +0530

    Create DecimalToBinary.java

---
+++ Java/DecimalToBinary.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter decimal number: ");
+        int decimal = scanner.nextInt();
+
+        String binary = Integer.toBinaryString(decimal);
+
+        System.out.println("Binary equivalent: " + binary);
+    }
+}
