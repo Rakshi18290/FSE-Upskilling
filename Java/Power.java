af905c9c7d19427b9e71a7fdf161397598b9004c
Rakshi18290 <vsrakshithablr@gmail.com>
2025-05-28 09:15:10 +0530

    Create Power.java

---
+++ Java/Power.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Power {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter base: ");
+        int base = scanner.nextInt();
+
+        System.out.print("Enter exponent: ");
+        int exponent = scanner.nextInt();
+
+        int result = 1;
+        for (int i = 1; i <= exponent; i++) {
+            result *= base;
+        }
+
+        System.out.println(base + " raised to " + exponent + " is: " + result);
+    }
+}
