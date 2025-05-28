4b55d9a8aa3fd88b9204ab0b99243363ce835ce1
Rakshi18290 <vsrakshithablr@gmail.com>
2025-05-28 20:00:58 +0530

    Create ChatServer.java

---
+++ Java/ChatServer.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.net.*;
+
+public class ChatServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(5000);
+        System.out.println("Server started. Waiting for client...");
+        Socket client = server.accept();
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+        out.println("Welcome!");
+
+        String line;
+        while ((line = in.readLine()) != null) {
+            System.out.println("Client: " + line);
+        }
+
+        client.close();
+        server.close();
+    }
+}
